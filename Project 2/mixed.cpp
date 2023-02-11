6bb3f72f958164b55594d74a5a757911857fa4d2
Hisen Zhang <hisen@hisenz.com>
2023-02-11 00:38:10 -0500

    formatting

--- Project 2/mixed.cpp
+++ Project 2/mixed.cpp
@@ -50,23 +50,23 @@ void mixed_mul(int blockSize)
 			{
 				for (i = ii; i < ii + blockSize && i < N; i++)
 				{
-					for (j = jj; j < jj + blockSize && j < N; j+=4)
+					for (j = jj; j < jj + blockSize && j < N; j += 4)
 					{
-						for (k = kk; k < kk + blockSize && k < N; k+=4)
+						for (k = kk; k < kk + blockSize && k < N; k += 4)
 						{
 							__m128 r = _mm_load_ps(&c[i][j]);
-                            __m128 s = _mm_load_ps(&a[i][k]);
-                            __m128 b_0 = _mm_load_ps(&b[k][j + 0]);
-                            __m128 b_1 = _mm_loadu_ps(&b[k][j + 1]);
-                            __m128 b_2 = _mm_loadu_ps(&b[k][j + 2]);
-                            __m128 b_3 = _mm_loadu_ps(&b[k][j + 3]);
+							__m128 s = _mm_load_ps(&a[i][k]);
+							__m128 b_0 = _mm_load_ps(&b[k][j + 0]);
+							__m128 b_1 = _mm_loadu_ps(&b[k][j + 1]);
+							__m128 b_2 = _mm_loadu_ps(&b[k][j + 2]);
+							__m128 b_3 = _mm_loadu_ps(&b[k][j + 3]);
 
-                            r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x00), b_0));
-                            r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x55), b_1));
-                            r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xaa), b_2));
-                            r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xff), b_3));
+							r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x00), b_0));
+							r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x55), b_1));
+							r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xaa), b_2));
+							r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xff), b_3));
 
-                            _mm_store_ps(&c[i][j], r);
+							_mm_store_ps(&c[i][j], r);
 						}
 					}
 				}
@@ -75,33 +75,31 @@ void mixed_mul(int blockSize)
 	}
 }
 
-
 void simd_mul()
 {
-    for (int i = 0; i < N; i += 1)
-    {
-        for (int j = 0; j < N; j += 4)
-        {
-            for (int k = 0; k < N; k += 4)
-            {
-                __m128 r = _mm_load_ps(&c[i][j]);
-                __m128 s = _mm_load_ps(&a[i][k]);
-                __m128 b_0 = _mm_load_ps(&b[k][j + 0]);
-                __m128 b_1 = _mm_loadu_ps(&b[k][j + 1]);
-                __m128 b_2 = _mm_loadu_ps(&b[k][j + 2]);
-                __m128 b_3 = _mm_loadu_ps(&b[k][j + 3]);
+	for (int i = 0; i < N; i += 1)
+	{
+		for (int j = 0; j < N; j += 4)
+		{
+			for (int k = 0; k < N; k += 4)
+			{
+				__m128 r = _mm_load_ps(&c[i][j]);
+				__m128 s = _mm_load_ps(&a[i][k]);
+				__m128 b_0 = _mm_load_ps(&b[k][j + 0]);
+				__m128 b_1 = _mm_loadu_ps(&b[k][j + 1]);
+				__m128 b_2 = _mm_loadu_ps(&b[k][j + 2]);
+				__m128 b_3 = _mm_loadu_ps(&b[k][j + 3]);
 
-                r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x00), b_0));
-                r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x55), b_1));
-                r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xaa), b_2));
-                r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xff), b_3));
+				r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x00), b_0));
+				r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0x55), b_1));
+				r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xaa), b_2));
+				r = _mm_add_ps(r, _mm_mul_ps(_mm_shuffle_ps(s, s, 0xff), b_3));
 
-                _mm_store_ps(&c[i][j], r);
-            }
-        }
-    }
+				_mm_store_ps(&c[i][j], r);
+			}
+		}
+	}
 }
-
 
 void tiled_mul(int blockSize)
 {
@@ -137,29 +135,29 @@ int main()
 	randomize(a);
 	randomize(b);
 
-    begin = clock();
+	begin = clock();
 	naive_mul();
 	end = clock();
 	elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
 	printf("Naive %.6f \n", elapsed);
 
-    begin = clock();
+	begin = clock();
 	mixed_mul(BS);
 	end = clock();
 	elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
 	printf("Mixed %.6f \n", elapsed);
 
-    begin = clock();
+	begin = clock();
 	simd_mul();
 	end = clock();
 	elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("SIMD %.6f \n", elapsed);	
+	printf("SIMD %.6f \n", elapsed);
 
 	begin = clock();
 	tiled_mul(BS);
 	end = clock();
 	elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("Tiled %.6f \n", elapsed);	
+	printf("Tiled %.6f \n", elapsed);
 
 	return EXIT_SUCCESS;
 }
