2700666ad9df0ab5065633fef8d6219f741e9045
Wyatt Marvil <2020wmarvil@gmail.com>
2023-04-16 10:45:41 -0400

    Fix SIMD

--- DSP/DSP.h
+++ DSP/DSP.h
@@ -3,216 +3,71 @@
 #include <algorithm>
 #include <array>
 #include <immintrin.h>
+#include <iostream>
+#include <memory>
 #include <numeric>
 #include <vector>
-#include <memory>
-
-// SIMD FIR Filter reference: https://thewolfsound.com/fir-filter-with-simd/
 
 // The number of floats that fit into an AVX register.
 constexpr uint32_t AVX_FLOAT_COUNT = 8u;
 
-struct alignas(AVX_FLOAT_COUNT * alignof(float)) avx_alignment_t {};
+float highestMultipleOfNIn(float in, float N) {
+    return static_cast<long long>(in / N);
+}
 
-template <typename SampleType, size_t alignment = alignof(SampleType)>
 struct FilterInput {
-    static constexpr auto VECTOR_SIZE = alignment / alignof(SampleType);
+    FilterInput(const std::vector<float>& inSignal, const std::vector<float>& filter) {
+        size_t alignment = 1u;
+        const auto minimalPaddedSize = inSignal.size() + 2 * filter.size() - 2u;
+        const auto alignedPaddedSize = alignment * (highestMultipleOfNIn(minimalPaddedSize - 1u, alignment) + 1u);
 
-    FilterInput(const std::vector<SampleType>& inputSignal, const std::vector<SampleType>& filter) {
-        const auto minimalPaddedSize = inputSignal.size() + 2 * filter.size() - 2u;
-        const auto alignedPaddedSize = VECTOR_SIZE * (highestMultipleOfNIn(minimalPaddedSize - 1u, VECTOR_SIZE) + 1u);
-        inputLength = alignedPaddedSize;
+        const auto inputLength = alignedPaddedSize;
+        this->signal.resize(inputLength, 0.f); // Pad input signal with zeros
+        std::copy(inSignal.begin(), inSignal.end(), this->signal.begin() + filter.size() - 1u);
 
-        inputStorage.reset(new avx_alignment_t[inputLength / AVX_FLOAT_COUNT]);
-        std::copy(inputSignal.begin(), inputSignal.end(), reinterpret_cast<float*>(inputStorage.get()) + filter.size() - 1u);
-        x = reinterpret_cast<float*>(inputStorage.get());
+        outputLength = inSignal.size() + filter.size() - 1u;
 
-        outputLength = inputSignal.size() + filter.size() - 1u;
-        outputStorage.resize(outputLength);
-        
-        filterLength = VECTOR_SIZE * (highestMultipleOfNIn(filter.size() - 1u, VECTOR_SIZE) + 1u);
-        reversedFilterCoefficientsStorage.resize(filterLength);
-        
-        std::reverse_copy(filter.begin(), filter.end(), reversedFilterCoefficientsStorage.begin());
-        for (auto i = filter.size(); i < reversedFilterCoefficientsStorage.size(); ++i) {
-            reversedFilterCoefficientsStorage[i] = 0.f;
-        }
-        
-        c = reversedFilterCoefficientsStorage.data();
-        filterLength = reversedFilterCoefficientsStorage.size();
-        y = outputStorage.data();
+        const auto filterLength = alignment * (highestMultipleOfNIn(filter.size() - 1u, alignment) + 1);
+        this->filter.resize(filterLength);
 
-        alignedStorageSize = reversedFilterCoefficientsStorage.size() + AVX_FLOAT_COUNT;
-        for (auto k = 0u; k < AVX_FLOAT_COUNT; ++k) {
-            alignedReversedFilterCoefficientsStorage[k].reset(new avx_alignment_t[alignedStorageSize / AVX_FLOAT_COUNT]);
-            cAligned[k] = reinterpret_cast<SampleType*>(alignedReversedFilterCoefficientsStorage[k].get());
-            
-            for (auto i = 0u; i < k; ++i) {
-                cAligned[k][i] = 0.f;
-            }
-            std::copy(reversedFilterCoefficientsStorage.begin(),
-                      reversedFilterCoefficientsStorage.end(),
-                      cAligned[k] + k);
-            for (auto i = reversedFilterCoefficientsStorage.size() + k; i < alignedStorageSize; ++i) {
-                cAligned[k][i] = 0.f;
-            }
+        // Reverse and pad with zeros
+        std::reverse_copy(filter.begin(), filter.end(), this->filter.begin());
+        for (auto i = filter.size(); i < this->filter.size(); ++i) {
+            this->filter[i] = 0.f;
         }
     }
 
-    std::vector<SampleType> output() {
-        auto result = outputStorage;
-        result.resize(outputLength);
-        return result;
-    }
-    
-    const SampleType* x;  // input signal
-    size_t inputLength;
-    const SampleType* c;  // reversed filter coefficients
-    size_t filterLength;
-    SampleType* y;  // output (filtered) signal
-    size_t outputLength;
-    size_t alignedStorageSize;
-    SampleType* cAligned[AVX_FLOAT_COUNT];
- private:
-    std::vector<float> reversedFilterCoefficientsStorage;
-    std::vector<float> outputStorage;
-    std::array<std::unique_ptr<avx_alignment_t[]>, AVX_FLOAT_COUNT> alignedReversedFilterCoefficientsStorage;
-    std::unique_ptr<avx_alignment_t[]> inputStorage;
+    std::vector<float> signal;
+    std::vector<float> filter;
+    uint32_t outputLength;
 };
 
+void applyFirFilter(const FilterInput& input, std::vector<float>& outSignal) {
+    outSignal.resize(input.outputLength);
 
-inline float* applyFirFilterSingle(const FilterInput<float>& input) {
-    const auto* x = input.x;
-    const auto* c = input.c;
-    auto* y = input.y;
-    
-    for (auto i = 0u; i < input.outputLength; ++i) {
-        y[i] = 0.f;
-        for (auto j = 0u; j < input.filterLength; ++j) {
-            y[i] += x[i + j] * c[j];
+    for (uint32_t i = 0; i < input.outputLength; ++i) {
+        outSignal[i] = 0.0f;
+        for (uint32_t j = 0; j < input.filter.size(); ++j) {
+            outSignal[i] += input.signal[i + j] * input.filter[j];
         }
     }
-
-    return y;
 }
 
-float* applyFirFilterAVX_innerLoopVectorization(FilterInput<float>& input) {
-    const auto* x = input.x;
-    const auto* c = input.c;
-    auto* y = input.y;
-    
-    // A fixed-size array to move the data from registers into
-    std::array<float, AVX_FLOAT_COUNT> outStore;
-    
-    for (auto i = 0u; i < input.outputLength; ++i) {
-        // Set a SIMD register to all zeros;
-        // we will use it as an accumulator
-        auto outChunk = _mm256_setzero_ps();
-        
-        // Note the increment
-        for (auto j = 0u; j < input.filterLength; j += AVX_FLOAT_COUNT) {
-            // Load the unaligned input signal data into a SIMD register
-            auto xChunk = _mm256_loadu_ps(x + i + j);
-            // Load the unaligned reversed filter coefficients 
-            // into a SIMD register
-            auto cChunk = _mm256_loadu_ps(c + j);
-            
-            // Multiply the both registers element-wise
-            auto temp = _mm256_mul_ps(xChunk, cChunk);
-            
-            // Element-wise add to the accumulator
-            outChunk = _mm256_add_ps(outChunk, temp);
-        }
-        
-        // Transfer the contents of the accumulator 
-        // to the output array
-        _mm256_storeu_ps(outStore.data(), outChunk);
-        
-        // Sum the partial sums in the accumulator and assign to the output
-        y[i] = std::accumulate(outStore.begin(), outStore.end(), 0.f);
-    }
-    
-    return y;
-}
+void applyFirFilterAVX(const FilterInput& input, std::vector<float>& outSignal) {
+    outSignal.resize(input.outputLength);
 
-float* applyFirFilterAVX_outerLoopVectorization(FilterInput<float>& input) {
-    const auto* x = input.x;
-    const auto* c = input.c;
-    auto* y = input.y;
-    
-    // Note the increment
-    for (auto i = 0u; i < input.outputLength; i += AVX_FLOAT_COUNT) {
-        // Set 8 computed outputs initially to 0
-        auto yChunk = _mm256_setzero_ps();
+    std::array<float, AVX_FLOAT_COUNT> result;
+    for (uint32_t i = 0; i < input.outputLength; ++i) {
+        __m256 accumulator = _mm256_setzero_ps();
         
-        for (auto j = 0u; j < input.filterLength; ++j) {
-            // Load an 8-element vector from x into an AVX register
-            auto xChunk = _mm256_loadu_ps(x + i + j);
-            
-            // Load c[j] filter coefficient into every variable
-            // of an 8-element AVX register
-            auto cChunk = _mm256_set1_ps(c[j]);
-            
-            // Element-wise multiplication
-            auto temp = _mm256_mul_ps(xChunk, cChunk);
-            
-            // Add to the accumulators
-            yChunk = _mm256_add_ps(yChunk, temp);
+        for (uint32_t j = 0; j < input.filter.size(); j += AVX_FLOAT_COUNT) {
+            __m256 signalLoad = _mm256_loadu_ps(input.signal.data() + i + j); // load signal and filter
+            __m256 filterLoad = _mm256_loadu_ps(input.filter.data() + j);
+            __m256 temp       = _mm256_mul_ps(signalLoad, filterLoad);        // element-wise multiply
+            accumulator       = _mm256_add_ps(accumulator, temp);             // element-wise add
         }
         
-        // Store 8 computed values in the result vector
-        _mm256_storeu_ps(y + i, yChunk);
+        _mm256_storeu_ps(result.data(), accumulator); // move the accumulator into the result and sum
+        outSignal[i] = std::accumulate(result.begin(), result.end(), 0.f);
     }
-    
-    return y;
-}
-
-float* applyFirFilterAVX_outerInnerLoopVectorization(FilterInput<float>& input) {
-  const auto* x = input.x;
-  const auto* c = input.c;
-  auto* y = input.y;
-
-    // An 8-element array for transferring data from AVX registers
-    std::array<float, AVX_FLOAT_COUNT> outStore;
-    
-    // 8 separate accumulators for each of the 8 computed outputs
-    std::array<__m256, AVX_FLOAT_COUNT> outChunk;
-    
-    // Note the increment
-    for (auto i = 0u; i < input.outputLength; i += AVX_FLOAT_COUNT) {
-        // Initialize the accumulators to 0
-        for (auto k = 0u; k < AVX_FLOAT_COUNT; ++k) {
-            outChunk[k] = _mm256_setzero_ps();
-        }
-        
-        // Note the increment
-        for (auto j = 0u; j < input.filterLength; j += AVX_FLOAT_COUNT) {
-            // Load filter coefficients into an AVX register
-            auto cChunk = _mm256_loadu_ps(c + j);
-            
-            for (auto k = 0u; k < AVX_FLOAT_COUNT; ++k) {
-                // Load the input samples into an AVX register
-                auto xChunk = _mm256_loadu_ps(x + i + j + k);
-                
-                // Element-wise multiplication
-                auto temp = _mm256_mul_ps(xChunk, cChunk);
-                
-                // Add to the dedicated accumulator
-                outChunk[k] = _mm256_add_ps(outChunk[k], temp);
-            }
-        }
-        
-        // Summarize the accumulators
-        for (auto k = 0u; k < AVX_FLOAT_COUNT; ++k) {
-            // Transfer the data into the helper array
-            _mm256_storeu_ps(outStore.data(), outChunk[k]);
-            
-            if (i + k < input.outputLength)
-                // Final sum for each of the 8 outputs 
-                // computed in 1 outer loop iteration
-                y[i + k] = std::accumulate(outStore.begin(), outStore.end(), 0.f);
-        }
-    }
-    
-    return y;
 }
--- DSP/main.cpp
+++ DSP/main.cpp
@@ -4,8 +4,101 @@
 
 #include <iostream>
 
+// TODO:
+// - Implement different filter types (echo, low pass, high pass, band pass, 
+// - Preset waveforms to use (test files and also saw/square/sine waves)
+// - Some kind of interface? imgui?
+
+#define TAU 6.2831855
+
+std::vector<float> GenerateSineWave(float amplitude, float frequency, int numSamples) {
+	std::vector<float> result(numSamples);
+
+	for (int i = 0; i < numSamples; i++)
+	{
+		result[i] = amplitude * std::sin(frequency * i * TAU);
+	}
+
+	return result;
+}
+
+void WriteSignal(const std::vector<float>& signal, const std::string& path) {
+	std::vector<std::vector<float>> channels = { signal };
+
+	AudioFile<float> af;
+	af.setAudioBuffer(channels);
+	af.save(path);
+}
+
 int main()
 {
-	std::cout << "hello world\n";
+	std::cout << "Loading signals...\n";
+
+	AudioFile<float> testSignal;
+	testSignal.load("data/input/test-audio.wav");
+	//thomasSignal.load("data/input/thomas.wav");
+	//std::vector<float> sine = GenerateSineWave(1.0f, 440.0f, 441000 * 5);
+
+	std::cout << "Loading filters...\n";
+	std::vector<float> impulseResponse = { -1.0f, 0.0f, 1.0f };
+	std::vector<float> derivativeImpulseResponse = {
+		-0.03926588615294601,
+		-0.05553547436862413,
+		-0.07674634732792313,
+		-0.10359512127942656,
+		-0.13653763745381112,
+		-0.17563154711765214,
+		-0.22037129189056356,
+		-0.26953979617440665,
+		-0.32110731416014476,
+		-0.37220987669580885,
+		-0.41923575145442643,
+		-0.45803722114653117,
+		-0.48426719093924325,
+		-0.493817805141109  ,
+		-0.483315193354812  ,
+		-0.4506055551298345 ,
+		-0.39515794257351333,
+		-0.31831200150042305,
+		-0.22331589501120253,
+		-0.11512890376975546,
+		0.0                 ,
+		0.11512890376975546 ,
+		0.22331589501120253 ,
+		0.31831200150042305 ,
+		0.39515794257351333 ,
+		0.4506055551298345  ,
+		0.483315193354812   ,
+		0.493817805141109   ,
+		0.48426719093924325 ,
+		0.45803722114653117 ,
+		0.41923575145442643 ,
+		0.37220987669580885 ,
+		0.32110731416014476 ,
+		0.26953979617440665 ,
+		0.22037129189056356 ,
+		0.17563154711765214 ,
+		0.13653763745381112 ,
+		0.10359512127942656 ,
+		0.07674634732792313 ,
+		0.05553547436862413 ,
+	};
+	std::vector<float> echoImpulseResponse;
+	for (int i = 0; i < 44100; i++) {
+		echoImpulseResponse.push_back(0.0f);
+	} echoImpulseResponse[0] = 1.0f; echoImpulseResponse.push_back(1.0f);
+
+	FilterInput testEchoFilter(testSignal.samples[0], echoImpulseResponse);
+
+	std::cout << "Applying filter (non-SIMD)...\n";
+	std::vector<float> result;
+	//applyFirFilter(testEchoFilter, result);
+	//WriteSignal(result, "data/out.wav");
+
+	std::cout << "Applying filter (SIMD)...\n";
+	result.clear();
+	applyFirFilterAVX(testEchoFilter, result);
+	WriteSignal(result, "data/outSIMD.wav");
+
 	return 0;
 }
