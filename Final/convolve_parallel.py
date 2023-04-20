3cac340baff16bcf7da14ca289b01c3b9de4d831
Hisen Zhang <hisen@hisenz.com>
2023-04-20 00:38:17 -0400

    format output

--- Final/convolve_parallel.py
+++ Final/convolve_parallel.py
@@ -1,19 +1,22 @@
 import numpy as np
 
-k=[1,2]
-a=list(range(10))
+k = [1, 2]
+a = list(range(10))
 
-print('ref\t',np.convolve(k,a,mode='full'))
-win = 4
+print('full\t', a, '*', k, '=', np.convolve(k, a, mode='full'))
 
-def frac(a,k):
-    buf = np.empty(len(k)+len(a)-1)
-    idx =0
+
+def partialConv(a, k, chunk):
+    print('chunk_size =', chunk)
+    print('---')
+    buf = np.zeros(len(k)+len(a)-1)
+    idx = 0
     while idx < len(a):
-        t = np.convolve(k,a[idx:idx+win],mode='full')
-        print('partial\t',t)
-        buf[idx:idx+win+1] += t
-        idx += win
-    print('result\t',buf)
+        t = np.convolve(k, a[idx:idx+chunk], mode='full')
+        print('partial\t', a[idx:idx+chunk], '*', k, '=', t)
+        buf[idx:idx+chunk+1] += t
+        idx += chunk
+    print('total\t', buf)
 
-frac(a,k)
+
+partialConv(a, k, 4)
